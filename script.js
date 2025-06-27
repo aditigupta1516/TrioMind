4d5c8d8e039d0a371e44fcda209c4c5ad593873b
aditigupta1516 <ananyaditi32@gmail.com>
2025-06-27 17:05:30 +0000

    js

--- script.js
+++ script.js
@@ -26,31 +26,31 @@ let isOpenCvReady = false;
 //voice control
 const SpeechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition();
 const Recognition= new SpeechRecognition();
-Recognition.continuous = true;
-Recognition.interimResults=false;
-Recognition.lang='en-US';
-Recognition.onresult= (event) =>{
-    const command = event.result[event.result.length-1][0].transcript.trim(),toLowerCase();
-    voiceIndicator.textContent= `Heard: ${command};
+recognition.continuous = true;
+recognition.interimResults=false;
+recognition.lang='en-US';
+recognition.onresult= (event) =>{
+    const command = event.results[event.results.length-1][0].transcript.trim().toLowerCase();
+    voiceIndicator.textContent= `Heard: ${command}`;
     if(command.includes("next")||command.includes("right")){
-    navigate 1;
-    }else-if(command.includes("previous")||command.includes("back")||command.includes("left")){
+    navigate(1);
+    }else if(command.includes("previous")||command.includes("back")||command.includes("left")){
     navigate(-1);
-    }else-if(command.includes("vitals")){
+    }else if(command.includes("vitals")){
     jumpToSection("vitals");
-    }else-if(command.includes("medications")){
+    }else if(command.includes("medications")){
     jumpToSection("medications");
-    }else-if(command.includes("history")){
+    }else if(command.includes("history")){
     jumpToSection("history");
-    }else-if(command.includes("lab")){
+    }else if(command.includes("lab")){
     jumpToSection("lab");
-    }else-if(command.includes("appointments")){
+    }else if(command.includes("appointments")){
     jumpToSection("appointments");
-    }else-if(command.includes("emergency")){
+    }else if(command.includes("emergency")){
     jumpToSection("emergency");
-    }else-if(command.includes("enable voice")){
+    }else if(command.includes("enable voice")){
     startVoiceControl();
-    }else-if(command.includes("disable voice")){
+    }else if(command.includes("disable voice")){
     stopVoiceControl();
     }
 };
